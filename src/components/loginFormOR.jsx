18229e709a2cf2df4f0037c70736d943d7dcae40
Sean <seanzzz@Seans-Mac-Studio.local>
2023-01-11 16:06:48 -0700

    extract form functions

---
+++ src/components/loginFormOR.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Joi from 'joi-browser'
+import Form from './common/form';
+import Input from './common/input';
+
+class LoginForm extends Form {
+    state = {
+        data: { username: '', password: '' },
+        errors: {}
+    };
+
+    schema = {
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().required().label('Password')
+    }
+
+
+    doSubmit = () => {
+        // Call the server  
+        console.log('Submitted');
+    }
+
+
+    render() {
+        const { data, errors } = this.state;
+
+        return (<div>
+            <h1 className="">Login</h1>
+            <form onSubmit={this.handleSubmit}>
+                <Input name="username" value={data.username} label='Username' onChange={this.handleChange} error={errors.username} />
+                <Input name="password" value={data.password} label='Password' onChange={this.handleChange} error={errors.password} />
+
+                <button disabled={this.validate} className="btn btn-primary">Login</button>
+            </form>
+        </div>);
+    }
+}
+
+export default LoginForm;
