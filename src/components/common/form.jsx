7060adc4e97200178ef7a2c9843e3bd8ab4cacca
Sean <seanzzz@Seans-Mac-Studio.local>
2023-01-12 15:35:58 -0700

    Add movieForm

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,5 +1,7 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Joi from "joi-browser";
+import Input from "./input";
+import Select from "./select";
 
 class Form extends Component {
   state = {
@@ -45,6 +47,44 @@ class Form extends Component {
 
     this.setState({ data, errors });
   };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderInput(name, label, type = "text") {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 }
 
 export default Form;
--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Input = ({ name, label, value, onChange, error }) => {
-    return (
-        <div className="form-group">
-            <label htmlFor={name}>{label}</label>
-            <input name={name} value={value} onChange={onChange} id={name} type="text" className="form-control" />
-            {error && <div className="alert alert-danger">{error}</div>}
-        </div>);
-}
+const Input = ({ name, label, error, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input {...rest} name={name} id={name} className="form-control" />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
 
 export default Input;
